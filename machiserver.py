cc4cda5386487210258c79a0a0b8c599c98d8ff8
alex <alexander.steen.gent@gmail.com>
2017-02-11 22:12:57 +0100

    wip

--- machiserver.py
+++ machiserver.py
@@ -40,9 +40,6 @@ commercialCards = {k:v for k,v in allCards.items() if v["type"] == "commercial"}
 majorCards =      {k:v for k,v in allCards.items() if v["type"] == "major"}      # purple
 landmarkCards =   {k:v for k,v in allCards.items() if v["type"] == "landmark"}   # yellow
 
-allActions = ["ndice", "reroll", "stealfrom", "tradewho", "tradewhat", "tradefor", "build"]
-allPhases = allActions + ["roll", "income"]
-
 class Table:
     """This guy manages the stuff on the table without knowing the rules of the game
     Ask it to do something it cannot, and it will throw an assert.
@@ -76,74 +73,101 @@ class Table:
         for playerId in playerIds:
             self.build(playerId, "wheatfield")
             self.build(playerId, "bakery")
-            self.takeFromBank(playerId, 3)
+            self.gainCash(playerId, 3)
+    def __str__(self):
+        return pprint.pformat({
+            "bank":self.bank,
+            "cities":self.cities,
+            "buildingbank":self.buildingBank})
+    def availableBuildings(self):
+        return [b for b,n in self.buildingBank.items() if n > 0]
     def build(self, playerId, building):
+        debug(playerId + " builds a " + building)
         if building in landmarkCards.keys():
             assert(building not in self.cities[playerId])
         else:
-            nInBank = self.buildingbank[building]
+            nInBank = self.buildingBank[building]
             assert(nInBank > 0)
-            self.buildingbank[building] = nInBank - 1
+            self.buildingBank[building] = nInBank - 1
         self.cities[playerId].append(building)
+    def swapBuildings(self, playerIdA, buildingA, playerIdB, builingB):
+        cityA = self.cities[playerIdA]
+        cityB = self.cities[playerIdB]
+        assert(buildingA in cityA and buildingB in cityB)
+        cityA.remove(buildingA)
+        cityB.remove(buildingB)
+        cityA.append(buildingB)
+        cityB.append(buildingA)
+        debug(playerIdA + " trades its " + buildingA + " with " + playerIdB + "'s " + buildingB)
     def looseCash(self, playerId, amount):
+        debug(playerId + " pays " + str(amount) + " to the bank")
         account = self.bank[playerId]
         assert(amount <= account)
         self.bank[playerId] = account - amount
     def gainCash(self, playerId, amount):
+        debug(playerId + " gets " + str(amount) + " from the bank")
         account = self.bank[playerId]
         self.bank[playerId] = account + amount
+    def transferCash(self, sourceId, targetId, amount):
+        debug(sourceId + " pays " + str(actualAmount) + " to " + targetId)
+        assert(amount >= self.bank[sourceId])
+        self.bank[sourceId] = account - amount
+        self.bank[targetId] = account + amount
     def getPlayerIds(self):
         return self.playerIds
     def playerHasN(self, playerId, building):
         assert(building in allCards)
         return self.cities[playerId].count(building)
+    def playerCredit(self, playerId):
+        return self.bank[playerId]
 
 class RandomDice():
     def roll(self, ndice):
         assert(0 < ndice <= 2)
         return [random.randint(1,6) for i in range(ndice)]
 
-class GameMaster:
+class Game:
     def __init__(self, players):
         self.playerIds = [p.getId() for p in players]
         assert(len(self.playerIds) == len(set(self.playerIds)) == 4) # check for duplicate playerIds
         self.currentPlayerIndex = 0
         self.playerDict = {p.getId():p for p in players}
         self.currentTurnNr = 0
-        self.currentPhase = allPhases[0]
-        self.turnState = {}
         self.table = Table(self.playerIds)
         self.dice = RandomDice()
     def getWinnerId(self):
         winners = []
         playerIds = self.table.getPlayerIds()
-        for playerId in playerIds
+        for playerId in playerIds:
             if all([self.table.playerHasN(playerId, landmark) for landmark in landmarkCards.keys()]):
                 winners.append(playerId)
         assert(len(winners) < 2) # two winners are not possible without having one first
         return None if len(winners) == 0 else winners[0]
     def _endTurn(self, extraTurn = False):
-        debug("==========  ending turn " + str(self.currentTurnNr) + "  ==========")
-        self.currentPhase = allPhases[0]
-        self.turnState = {} # clear all state
+        debug("==========   ending turn " + str(self.currentTurnNr) + "   ==========")
         self.currentTurnNr += 1
         if not extraTurn:
             self.currentPlayerIndex = (self.currentPlayerIndex + 1) % len(self.playerIds)
     def play(self):
-        player = self.playerDict[self.playerIds[self.currentPlayerIndex]]
-        roll = self._rollPhase()
-        # if you roll doubles and have an amusement park you get an extra turn
-        extraTurn = (     self.table.playerHasN(playerId,"amusementpark")
-                      and len(roll) == 2
-                      and roll[0] == roll[1] )
-        if extraTurn:
-            debug(playerId + " threw doubles, so gets another turn after this one!")
+        while self.getWinnerId() == None:
+            playerId = self.playerIds[self.currentPlayerIndex]
+            player = self.playerDict[playerId]
+            debug(str(self.table))
+            debug("==========  starting turn " + str(self.currentTurnNr) + "  ==========")
+            debug("-> " + playerId + "'s turn")
+            roll = self._rollPhase(player)
+            # if you roll doubles and have an amusement park you get an extra turn
+            extraTurn = (     self.table.playerHasN(playerId,"amusementpark")
+                          and len(roll) == 2
+                          and roll[0] == roll[1] )
+            if extraTurn:
+                debug(playerId + " threw doubles, so gets another turn after this one!")
 
-        self._calcIncome(player, roll)
-        self._satanPhase(player, roll) #will only do smth on roll == 6
-        self._buildPhase(player)
+            self._calcIncome(player, roll)
+            self._satanPhase(player, roll) #will only do smth on roll == 6
+            self._buildPhase(player)
 
-        self._endTurn(extraTurn = extraTurn)
+            self._endTurn(extraTurn = extraTurn)
 
     def _rollPhase(self, player):
         playerId = player.getId()
@@ -153,9 +177,9 @@ class GameMaster:
             choice = player.chooseAction(request)
             if choice == "2":
                 ndice = 2
-        roll = dice.roll(ndice)
+        roll = self.dice.roll(ndice)
         debug("rolled " + str(roll))
-        if self.playerHasN(playerId, "radiotower") > 0:
+        if self.table.playerHasN(playerId, "radiotower") > 0:
             request = {"action":"reroll", "lastroll":roll, "options":["y","n"]}
             choice = player.chooseAction(request)
             if choice == "y":
@@ -166,27 +190,27 @@ class GameMaster:
     def _satanPhase(self, player, roll):
         if sum(roll) == 6:
             playerId = player.getId()
-            if self.playerHasN(playerId, "stadium") > 0:
+            if self.table.playerHasN(playerId, "stadium") > 0:
                 for dupee in self._getPayOrder():
                     self.table._tryMoveCash(dupee, playerId, 2)
 
             if self.table.playerHasN(playerId, "tvstation") > 0:
                 orderedOtherIds = self._getPayOrder()  # same relative order for everyone
-                accounts = [(p,self.data["cash"][p]) for p in orderedOtherIds]
+                accounts = [(p,self.table.playerCredit(p)) for p in orderedOtherIds]
                 playersWithCash = [p for p,amount in accounts if amount > 0]
                 if playersWithCash:
                     request = {"action":"steal", "options":playersWithCash + [""]}
                     choice = player.chooseAction(request)
                     if choice != "":
                         assert(choice in playersWithCash)
-                        self.tryMoveCash(choice, playerId, 5)
+                        self._tryMoveCash(choice, playerId, 5)
 
-            if self.playerOwnsN(playerId, "businesscenter") > 0:
-                orderedOtherIds = self.getPayOrder()  # same relative order for everyone
+            if self.table.playerHasN(playerId, "businesscenter") > 0:
+                orderedOtherIds = self._getPayOrder()  # same relative order for everyone
                 tradableCards = ( set(allCards.keys())
                                 - set(landmarkCards.keys())
                                 - set(majorCards.keys()))
-                cities = self.data["city"]
+                cities = {pId:self.table.getCity(pId) for p in self.playerIds}
 
                 # tradewho
                 playersWithTradableCards = \
@@ -194,11 +218,11 @@ class GameMaster:
                                    if tradableCards.intersection(cities[otherPlayerId])]
                 if playersWithTradableCards:
                     request = {"action":"tradewho", "options":playersWithTradableCards + [""]}
-                    victim = player.chooseAction(request)
-                    if victim != "":
-                        assert(victim in playersWithTradableCards)
+                    victimId = player.chooseAction(request)
+                    if victimId != "":
+                        assert(victimId in playersWithTradableCards)
                         # tradewhat
-                        tradeWhatOptions = list(tradableCards.intersection(cities[victim]))
+                        tradeWhatOptions = list(tradableCards.intersection(cities[victimId]))
                         request = {"action":"tradewhat", "options":tradeWhatOptions + [""]}
                         stolen = player.chooseAction(request)
                         if stolen != "":
@@ -209,129 +233,92 @@ class GameMaster:
                             given = player.chooseAction(request)
                             if given != "":
                                 assert(given in tradeForOptions)
-                                self.swapBuildings(playerId, given, victim, stolen)
+                                self.table.swapBuildings(playerId, given, victimId, stolen)
 
-    def buildPhase(self, player):
+    def _buildPhase(self, player):
         playerId = player.getId()
-        buildings = self.getBuildableBuildings(playerId)
+        buildings = self._getBuildableBuildings(playerId)
         if len(buildings) > 0: 
             request = {"action":"build", "options":buildings + [""]}
             choice = player.chooseAction(request)
             if choice in buildings:
-                self.build(playerId, choice)
+                self._buyBuilding(playerId, choice)
 
     def getCurrentPlayerId(self):
         return self.playerIds[self.currentPlayerIndex]
 
-
-    def playerOwnsN(self, playerId, building):
-        return self.data["city"][playerId].count(building)
-
-    def getPayOrder(self):
+    def _getPayOrder(self):
         i = self.currentPlayerIndex
         result = self.playerIds[i+1:] + self.playerIds[:i]
         result.reverse()
         return result
 
-    def calcIncome(self, roll):
-        playerId = self.getCurrentPlayerId()
+    def _calcIncome(self, player, roll):
+        playerId = player.getId()
         # first red (commercial buildings, you'll pay for this)
         # also affected by shopping mall bonus
         for cardName, cardData in commercialCards.items():
             if roll in cardData["roll"]:
                 basicAmount = cardData["amount"]
-                for luckyBastardId in self.getPayOrder():
-                    n = self.playerOwnsN(luckyBastardId, cardName)
-                    shoppingmallBonus = 1 if self.playerOwnsN(luckyBastardId, cardName) else 0
+                for luckyBastardId in self._getPayOrder():
+                    n = self.table.playerHasN(luckyBastardId, cardName)
+                    shoppingmallBonus = 1 if self.table.playerHasN(luckyBastardId, cardName) else 0
                     if n > 0:
-                        self.tryMoveCash(playerId, luckyBastardId, n * (basicAmount + shoppingmallBonus))
+                        self._tryMoveCash(playerId, luckyBastardId, n * (basicAmount + shoppingmallBonus))
 
         # then green (you throw your own nr) (green is secondary + multipliers)
         #   regular secondary cards (bakery and convenience store)
         #   also affected by shoppingmall bonus
         for cardName, cardData in secondaryCards.items():
             if roll in cardData["roll"]:
-                n = self.playerOwnsN(playerId, cardName)
-                shoppingmallBonus = 1 if self.playerOwnsN(playerId, cardName) else 0
+                n = self.table.playerHasN(playerId, cardName)
+                shoppingmallBonus = 1 if self.table.playerHasN(playerId, cardName) else 0
                 if n > 0:
                     basicAmount = cardData["amount"]
-                    self.playerGainsCash(playerId, n * (basicAmount + shoppingmallBonus))
+                    self.table.gainCash(playerId, n * (basicAmount + shoppingmallBonus))
 
         # green bis
         #   multiplier cards (cheese factory, furniture factory and market)
         # not affected by shopping mall bonus
         for cardName, cardData in multiplierCards.items():
-            if roll in cardData["roll"] and self.playerOwnsN(playerId, cardName):
+            if roll in cardData["roll"] and self.table.playerHasN(playerId, cardName):
                 cofactors = cardData["cofactor"]
-                n = sum([self.playerOwnsN(playerId, c) for c in cofactors])
+                n = sum([self.table.playerHasN(playerId, c) for c in cofactors])
                 if n > 0:
-                    self.playerGainsCash(playerId, n * cardData["factor"])
+                    self.table.gainCash(playerId, n * cardData["factor"])
 
         # then blue (anyone benefits from any throw)
         for cardName, cardData in primaryCards.items():
             if roll in cardData["roll"]:
                 for anyPlayerId in self.playerIds:
-                    n = self.playerOwnsN(anyPlayerId, cardName)
+                    n = self.table.playerHasN(anyPlayerId, cardName)
                     if n > 0:
-                        self.playerGainsCash(anyPlayerId, n * cardData["amount"])
+                        self.table.gainCash(anyPlayerId, n * cardData["amount"])
 
         #purple not handled here as it needs player input
 
-    def tryMoveCash(self, sourceId, targetId, amount):
-        sourceN = self.data["cash"][sourceId]
-        targetN = self.data["cash"][targetId]
+    def _tryMoveCash(self, sourceId, targetId, amount):
+        sourceN = self.table.playerCredit[sourceId]
+        targetN = self.table.playerCredit[targetId]
         actualAmount = min(amount, sourceN)
-        self.data["cash"][sourceId] = sourceN - actualAmount
-        self.data["cash"][targetId] = targetN + actualAmount
-        debug(sourceId + " pays " + str(actualAmount) + " to " + targetId)
+        if actualAmount > 0:
+            self.table.transferCash(sourceId, targetId, actualAmount)
 
-    def playerGainsCash(self, playerId, amount):
-        currentN = self.data["cash"][playerId]
-        self.data["cash"][playerId] = currentN + amount
-        debug(playerId + " gets " + str(amount) + " from the bank")
-
-    def swapBuildings(self, playerIdA, buildingA, playerIdB, buildingB):
-        cityA = self.data["city"][playerIdA]
-        cityB = self.data["city"][playerIdB]
-        assert(buildingA in cityA and buildingB in cityB)
-        cityA.remove(buildingA)
-        cityB.remove(buildingB)
-        cityA.append(buildingB)
-        cityB.append(buildingA)
-        debug(playerIdA + " trades its " + buildingA + " with " + playerIdB + "'s " + buildingB)
-
-    def nextTurn(self, extraTurn = False):
-        debug("==========  ending turn " + str(self.currentTurnNr) + "  ==========")
-        self.currentTurnNr += 1
-        if not extraTurn:
-            self.currentPlayerIndex = ( self.currentPlayerIndex + 1 ) % len(self.playerIds)
-
-    def getBuildableBuildings(self, playerId):
+    def _getBuildableBuildings(self, playerId):
         # first landmarks not yet built (so the YesToAllPlayer favors landmarks)
-        available =  set([b for b in landmarkCards.keys() if self.playerOwnsN(playerId, b) == 0])
+        available =  set([b for b in landmarkCards.keys() if self.table.playerHasN(playerId, b) == 0])
         # add all building types available in the bank...
-        available.update([b for b,n in self.data["buildingbank"].items() if n > 0])
+        available.update(self.table.availableBuildings())
         # ... excluding the major building type buildings allready build by player
-        available.difference_update([b for b in majorCards.keys() if self.playerOwnsN(playerId, b) > 0])
-
+        available.difference_update([b for b in majorCards.keys() if self.table.playerHasN(playerId, b) > 0])
         # return all these that the player can afford
-        cash = self.data["cash"][playerId]
+        cash = self.table.playerCredit(playerId)
         return [b for b in available if cash >= allCards[b]["cost"]]
 
-    def build(self, playerId, building):
-        if building in landmarkCards.keys():
-            assert(self.playerOwnsN(playerId, building) == 0)
-        else:
-            nInBank = self.data["buildingbank"][building]
-            assert(nInBank > 0)
-            self.data["buildingbank"][building] = nInBank - 1
-        self.data["city"][playerId].append(building)
-
-        cash = self.data["cash"][playerId]
+    def _buyBuilding(self, playerId, building):
         cost = allCards[building]["cost"]
-        assert(cash >= cost)
-        self.data["cash"][playerId] = cash - cost
-        debug(playerId + " builds a " + building)
+        self.table.looseCash(playerId,cost)
+        self.table.build(playerId, building)
 
 class Player:
     def __init__(self, playerId):
@@ -369,29 +356,14 @@ class CafeBot(Player):
         return choice
 
 if __name__ == "__main__":
-    random.seed()
+    random.seed(1)
     players = [YesToAllBot("YesBot"), CafeBot("CafeBot"),
                RandomBot("RandomBot1"), RandomBot("RandomBot2")]
     playerIds = [p.getId() for p in players]
     
     score = {p.getId():0 for p in players}
     for i in range(10):
-        table = Table(playerIds)
-        gameState = GameState(playerIds)
-        while not GameMaster.getWinnerId(table, gameState):
-            currentPlayer = players[gameState.getCurrentPlayerId()]
-            if GameMaster.requiresChoice(table, gameState):
-                actionRequest = GameMaster.createActionRequest(table, gameState)
-                response = currentPlayer.chooseAction(actionRequest)
-                gameState
-            elif GameMaster.requiresRoll(table, gameState):
-                GameMaster
-                pass
-            else
-            action = GameMaster.nextAction(table, gameState)
-            if action.requiresDecision()
-            actionRequest = GameMaster.createActionRequest(table,gameState)
-            response = currentPlayer.chooseAction(actionRequest)
-            GameMaster.applyAction(table, gameState, actionRequest, response)
-        score[GameMaster.getWinnerId(table, gameState)] += 1
+        game = Game(players)
+        game.play()
+        score[game.getWinnerId()] += 1
         print "score = " + str(score)
