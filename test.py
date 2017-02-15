cbded5b615c7024e8d1f608d69c8f12b8ba751e8
alex <alexander.steen.gent@gmail.com>
2017-02-15 22:36:56 +0100

    wrap bots in test with api schema validators

--- test.py
+++ test.py
@@ -6,6 +6,21 @@ import time
 import gamemaster
 import machiplayers
 
+import json
+from jsonschema import validate
+
+validateApi = True
+
+class ApiValidator:
+    def __init__(self, bot, apiSchema):
+        self.bot = bot
+        self.apiSchema = apiSchema
+    def getId(self):
+	return self.bot.getId()
+    def chooseAction(self, actionRequest):
+	validate(actionRequest, apiSchema)
+        return self.bot.chooseAction(actionRequest)
+
 if __name__ == "__main__":
 
     botcommand = "bottemplate/pythonmachibot.py"
@@ -23,9 +38,14 @@ if __name__ == "__main__":
         machiplayers.HTTPBot("HTTPBot", "http://127.0.0.1:1234")
         ]
 
+    if validateApi:
+        with open("docs/api.schema.json","r") as f:
+            apiSchema = json.loads(f.read())
+        players = [ApiValidator(p, apiSchema) for p in players]
+
     playerIds = [p.getId() for p in players]
     score = {p.getId():0 for p in players}
-    for i in range(20):
+    for i in range(3):
         game = gamemaster.Game(players)
         game.play()
         score[game.getWinnerId()] += 1
