d104ce95a43c84f66bcea9fac78457e9b73f1320
sasja <alexander.steen.gent@gmail.com>
2017-02-23 20:46:19 +0100

    locking when manipulating file
    i realise this is nasty but it works for now, just got to get it up
    and running first

--- ranking/ranking.py
+++ ranking/ranking.py
@@ -1,59 +1,55 @@
 #!/usr/bin/env python
 import json
 import os.path
+import simpleflock
 
 class Ranking:
     def __init__(self, fn):
         self.fn = fn
+        self._lockfn = ".machilock"
         if os.path.isfile(fn):
-            with open(fn, "w+") as f:
-                # test contents
+            with open(fn, "r") as f:
                 data = json.load(f)
         else:
-            with open(fn, "w+") as f:
-                # create empty json
-                f.write("[]")
-            
-    def _atomicOp(self, operation, *args, **kwargs):
-        with open(self.fn, "w+") as f:
-            data = json.load(f)
-            operation(data, *args, **kwargs)
-            json.dump(data, f)
+            with open(fn, "w") as f:
+                json.dump({},f)
 
-    def _getData(self):
-        with open(self.fn, "w+") as f:
-            data = json.load(f)
-        return data
-
-    def _addEntry(self, data, url, branch, commit, score):
-        entryId = str(abs(hash((url,branch,commit))))
-        data[entryId] = {
-                "url":url,
-                "branch":branch,
-                "commit":commit,
-                "score":score }
-
-    def addEntry(self, *args, **kwargs):
-        self._atomicOp(self._addEntry, *args, **kwargs)
+    def addEntry(self, url, branch, commit, score):
+        with simpleflock.SimpleFlock(self._lockfn):
+            entryId = str(abs(hash((url,branch,commit))))
+            with open(self.fn, "r") as f:
+                data = json.load(f)
+            data[entryId] = {
+                    "url":url,
+                    "branch":branch,
+                    "commit":commit,
+                    "score":score }
+            with open(self.fn, "w") as f:
+                json.dump(data, f)
 
     def getEntryId(self, url, branch, commit):
-        data = self._getData()
-        matches = [entryId for entryId,e in data.items() if (
-                e["url"] == url
-            and e["branch"] == branch
-            and e["commit"] == commit )]
-        assert len(matches) == 1
-        return matches[0]
+        with simpleflock.SimpleFlock(self._lockfn):
+            with open(self.fn, "r") as f: data = json.load(f)
+            matches = [entryId for entryId,e in data.items() if (
+                    e["url"] == url
+                and e["branch"] == branch
+                and e["commit"] == commit )]
+            assert len(matches) == 1
+            return matches[0]
 
-    def _updateScore(self, data, entryId, newScore):
-        data[entryId]["score"] = newScore
-
-    def updateScore(self, *args, **kwargs):
-        self._atomicOp(self._updateScore, *args, **kwargs)
+    def updateScore(self, data, entryId, newScore):
+        with simpleflock.SimpleFlock(self._lockfn):
+            with open(self.fn, "r") as f:
+                data = json.load(f)
+            data[entryId]["score"] = newScore
+            with open(self.fn, "w") as f:
+                json.dump(data, f)
 
     def getRanking(self):
-        data = self._getData()
-        ranking = sorted(data.values(), key=lambda x: x["score"], reverse=True)
-        return [(i["score"],
-                 i["url"] + "_" + i["branch"] + "_" + i["commit"][:10])
-                for i in ranking]
+        with simpleflock.SimpleFlock(self._lockfn):
+            with open(self.fn, "r") as f:
+                data = json.load(f)
+            ranking = sorted(data.values(), key=lambda x: x["score"], reverse=True)
+            return [(i["score"],
+                     i["url"] + "_" + i["branch"] + "_" + i["commit"][:10])
+                    for i in ranking]
